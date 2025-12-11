8fb8842bc4bb3c491a1ce7b4d75793d90f47a886
blaire-glitch <blairaloo@gmail.com>
2025-12-12 02:59:46 +0300

    Add user site-packages to WSGI path for PyJWT

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,11 @@
 import sys
 import os
 
+# Add user site-packages to path (for pip --user installed packages)
+user_site_packages = '/home/Monsiuer/.local/lib/python3.10/site-packages'
+if user_site_packages not in sys.path:
+    sys.path.insert(0, user_site_packages)
+
 # Add your project directory to the sys.path
 project_home = '/home/Monsiuer/KAYO'
 if project_home not in sys.path:
